379ce06e99cf8bed8e67784ccc29a3e74576432e
Volodymyr Holovetskyi <vholovetskyi@gmail.com>
2024-10-11 22:56:32 +0200

    first commit

---
+++ src/main/java/ua/vholovetskyi/splitup/SplitUpApplication.java
@@ -0,0 +1,13 @@
+package ua.vholovetskyi.splitup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitUpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SplitUpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/vholovetskyi/splitup/SplitUpApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.vholovetskyi.splitup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitUpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
